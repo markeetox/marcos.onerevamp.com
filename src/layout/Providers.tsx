77246f8792055a36be42fed967e3e51d667a6c71
Marcos Kwasniewski <madaku@gmail.com>
2026-05-30 12:05:06 -0400

    deleted all

--- src/layout/Providers.tsx
+++
@@ -1,6 +0,0 @@
-import { Provider } from 'react-redux';
-import store from '../components/redux/store';
-
-export default function Providers({ children }: { children: React.ReactNode }) {
-   return <Provider store={store}>{children}</Provider>;
-}
